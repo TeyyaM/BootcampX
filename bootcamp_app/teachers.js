a14ce1c5898c04ddfa28ad92f54733f28875a62e
Tessa Azar MacLean <tessaamac@gmail.com>
2021-04-01 01:48:46 +0000

    parameterized queries to defend from SQL injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, 
 cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = assistance_requests.teacher_id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name = $1
+ORDER BY teacher;`;
+
+
+const value = [process.argv[2]];
+
+pool.query(queryString, value)
   .then(res => {
     res.rows.forEach(assist => {
       console.log(`${assist.cohort}: ${assist.teacher}`)
